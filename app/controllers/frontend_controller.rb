46b4767357e25ff80921a7fb4d63a270914c28b8
Sergio del Rio <sdelrio0@gmail.com>
2018-02-01 12:29:34 -0600

    Working implementation of MaterialUI

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -1,6 +1,6 @@
 # Handles server-side rendering React
 class FrontendController < ApplicationController
   def index
-    render component: 'index', props: { name: 'Sergio' }, prerender: true
+    render component: 'index', props: {}, prerender: false # TODO: Why setting to true crashes?
   end
 end
