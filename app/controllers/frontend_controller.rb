40637cd76c3c0f50ae90ddbf930c3028a105a2c0
Sergio del Rio <sdelrio0@gmail.com>
2018-02-01 12:31:51 -0600

    Update rubocop rule back to 80 lines

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -1,6 +1,7 @@
 # Handles server-side rendering React
 class FrontendController < ApplicationController
   def index
-    render component: 'index', props: {}, prerender: false # TODO: Why setting to true crashes?
+    # TODO: Why setting to true crashes?
+    render component: 'index', props: {}, prerender: false
   end
 end
